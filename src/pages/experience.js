7bd90ba32b514299659c49d68f2e6e8e0794bbff
byrockstarz <ballzznatthaphon@hotmail.com>
2021-08-20 23:25:33 +0700

    edit home , experience

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -4,62 +4,61 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { Container } from "reactstrap";
-import StyledExp from "../styles/home";
+import { MdLibraryBooks, MdWork } from "react-icons/md";
+import data from "../resume.json";
 
 const ExperiencePage = () => {
   return (
-    <Container fluid id="sectionexp">
-      <StyledExp>
-        <VerticalTimeline>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{
-              background: "rgb(33, 150, 243)",
-              color: "black",
-            }}
-            contentArrowStyle={{
-              borderRight: "7px solid  rgb(33, 150, 243)",
-            }}
-            date="2011 - present"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "black" }}
-          >
-            <h3 className="vertical-timeline-element-title">
-              ตำแหน่งงานปัจจุบัน
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              สถานที่ทำงานปัจจุบัน
-            </h4>
-            <p>รายละเอียดของงาน</p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2010 - 2011"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "black" }}
-          >
-            <h3 className="vertical-timeline-element-title">
-              ตำแหน่งงานปัจจุบัน
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              สถานที่ทำงานปัจจุบัน
-            </h4>
-            <p>รายละเอียดของงาน</p>
-          </VerticalTimelineElement>
-
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2013 - 2017"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "black" }}
-          >
-            <h3 className="vertical-timeline-element-title">
-              Study Computer Engineering
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Khon Kaen , Thailand
-            </h4>
-            <p>Rajamangala University Of Technology Isan Khon Kaen</p>
-          </VerticalTimelineElement>
-        </VerticalTimeline>
-      </StyledExp>
+    <Container fluid>
+      {data.map((basic, index) => {
+        console.log(basic);
+        return (
+          <VerticalTimeline key={index}>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date="2011 - present"
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<MdWork />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                {basic.basics.name}
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                {basic.basics.location.city}
+              </h4>
+              <p></p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date="2010 - 2011"
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<MdLibraryBooks />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                {basic.education.institution}
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                {basic.education.studyType}
+              </h4>
+              <p>{basic.education.score}</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date="2008 - 2010"
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<MdLibraryBooks />}
+            >
+              <h3 className="vertical-timeline-element-title"></h3>
+              <h4 className="vertical-timeline-element-subtitle"></h4>
+              <p></p>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        );
+      })}
     </Container>
   );
 };
